3b48bbe67777a10b33c505b813c81176adfa1d5d
CaT Programmer <phatcharapol2@gmail.com>
2024-06-25 19:16:02 +0700

    Add Header

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('aboutus');
+});
+
+Route::get('blog', function () {
+    return "<h1>หน้าบทความ</h1>";
+});
+
+Route::get('blog/{name}', function ($name) {
+    return "<h1>หน้าบทความ $name </h1>";
+});
+
+Route::get('login', function () {
+    return "<a href='".route('login')."'>Login</a>";
+});
+
+Route::get('admin/user/ace', function () {
+    return "<h1>สวัสดี Admin </h1>";
+})->name('login');
+
+Route::fallback(function(){
+    return "Not Found 404";
+});
