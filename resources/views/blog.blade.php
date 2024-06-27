6fa63aaaa249823a8b41b12a9104e4321abbac84
CaT Programmer <phatcharapol2@gmail.com>
2024-06-27 16:02:16 +0700

    Create table for control the blogs

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,11 +1,41 @@
 @extends('layout')
 @section('title', 'หน้าบทความทั้งหมด')
 @section('content')
-    <h1>บทความทั้งหมด</h1>
+    <h1>
+        บทความทั้งหมด</h1>
+    <hr>
+    <table class="table text-center">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">ชื่อบทความ</th>
+                <th scope="col">เนื้อหา</th>
+                <th scope="col">สถานะ</th>
+            </tr>
+        </thead>
+        @foreach ($blogs as $blog)
+            <tbody>
+                <tr>
+                    <th scope="row">{{ $blog['id'] }}</th>
+                    <td>{{ $blog['title'] }}</td>
+                    <td>{{ $blog['content'] }}</td>
+                    <td>
+                        @if ($blog['status'] === true)
+                            <a href="#" class="btn btn-success">เผยแพร่</a>
+                        @elseif($blog['status'] === false)
+                            <a href="#" class="btn btn-warning">ฉบับร่าง</a>
+                        @else
+                            {{ $blog['status'] }}
+                        @endif
+                    </td>
+                </tr>
+        @endforeach
+        </tbody>
+    </table>
     <hr>
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($blogs as $blog)
-        @if ($blog['status']==true)
+        @if ($blog['status'] == true)
             <div class="col">
                 <div class="card h-100">
                     <img src="/images/placeholder.webp" class="card-img-top" alt="{{ $blog['title'] }}">
