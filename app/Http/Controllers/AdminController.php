bba319e2909392c1905a01698f51635db5868151
CaT Programmer <phatcharapol2@gmail.com>
2024-06-28 15:51:28 +0700

    Using Query Builder

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,50 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     function index()
     {
-        $blogs = [
-            [
-                'id' => 1,
-                'title' => "บทความที่ 1",
-                'content' => "เนื้อหาบทความที่ 1",
-                'status' => true,
-            ],
-            [
-                'id' => 2,
-                'title' => "บทความที่ 2",
-                'content' => "เนื้อหาบทความที่ 2",
-                'status' => true,
-            ],
-            [
-                'id' => 3,
-                'title' => "บทความที่ 3",
-                'content' => "เนื้อหาบทความที่ 3",
-                'status' => true,
-            ],
-            [
-                'id' => 4,
-                'title' => "บทความที่ 4",
-                'content' => "เนื้อหาบทความที่ 4",
-                'status' => true,
-            ],
-            [
-                'id' => 5,
-                'title' => "บทความที่ 5",
-                'content' => "เนื้อหาบทความที่ 5",
-                'status' => true,
-            ],
-            [
-                'id' => 6,
-                'title' => "บทความที่ 6",
-                'content' => "เนื้อหาบทความที่ 6",
-                'status' => false,
-            ],
-        ];
-        return view('blog', compact('blogs'));
+        $blogs = DB::table('blogs')->get();
+        return view('blog', ['blogs' => $blogs]);
     }
 
     function about()
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,27 +6,27 @@
     <hr>
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($blogs as $blog)
-        @if ($blog['status'] == true)
+        @if ($blog->status == true)
             <div class="col">
                 <div class="card h-100">
-                    <img src="/images/placeholder.webp" class="card-img-top" alt="{{ $blog['title'] }}">
+                    <img src="/images/placeholder.webp" class="card-img-top" alt="{{ $blog->title }}">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $blog['title'] }}</h5>
-                        <p class="card-text">{{ $blog['content'] }}</p>
+                        <h5 class="card-title">{{ $blog->title }}</h5>
+                        <p class="card-text">{{ Str::limit($blog->content,100) }}</p>
                         <p class="text-success">เผยแพร่</p>
-                        <a href="{{route('blogid', $blog['id'])}}" class="btn btn-primary">ดูเพิ่มเติม</a>
+                        <a href="{{route('blogid', $blog->id)}}" class="btn btn-primary">ดูเพิ่มเติม</a>
                     </div>
                 </div>
             </div>
             @else
             <div class="col">
                 <div class="card h-100">
-                    <img src="/images/placeholder.webp" class="card-img-top" alt="{{ $blog['title'] }}">
+                    <img src="/images/placeholder.webp" class="card-img-top" alt="{{ $blog->title }}">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $blog['title'] }}</h5>
-                        <p class="card-text">{{ $blog['content'] }}</p>
+                        <h5 class="card-title">{{ $blog->title }}</h5>
+                        <p class="card-text">{{ Str::limit($blog->content,100) }}</p>
                         <p class="text-danger">ฉบับร่าง</p>
-                        <a href="{{route('blogid', $blog['id'])}}" class="btn btn-primary">ดูเพิ่มเติม</a>
+                        <a href="{{route('blogid', $blog->id)}}" class="btn btn-primary">ดูเพิ่มเติม</a>
                     </div>
                 </div>
             </div>
